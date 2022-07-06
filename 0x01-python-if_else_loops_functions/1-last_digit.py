bd301f6c28b5fde3567205373476055d40e996b8
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-06 23:44:39 +0300

    0x01. Python - if/else, loops, functions Commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 digit = abs(number) % 10
-if number < 0
+if number < 0:
     digit = -digit
 print(f"Last digit of {number:d} is {digit:d} and is ", end="")
 if digit > 5:
