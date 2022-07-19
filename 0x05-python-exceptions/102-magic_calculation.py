c80176b1eafb7cf125b3c7df4e0fbd01daa2d4cf
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-19 23:27:34 +0300

    0x05-python-exceptions project commit

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except:
+        except Exception:
             result = b + a
             break
     return (result)
