036e0e6a2e5041ab160a2c0275c9dfc0ddf5775b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-19 22:46:17 +0300

    0x06-python-classes project commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,9 +12,9 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        try:
-            self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
