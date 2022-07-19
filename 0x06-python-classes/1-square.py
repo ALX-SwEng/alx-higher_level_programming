34489827526b52a48a01a7aeea0130468f0f6db0
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-19 22:36:47 +0300

    0x06-python-classes project commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square. 
+    The class has one private instance variable ."""
+   
+    def __int__(self, size):
+        self.__size = size
