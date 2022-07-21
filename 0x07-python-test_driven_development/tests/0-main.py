f1e3861e8d50dda2210fced7a1cdfbeece9ffd14
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-21 14:47:35 +0300

    update 0-main.py

--- 0x07-python-test_driven_development/tests/0-main.py
+++ 0x07-python-test_driven_development/tests/0-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-add_integer = __import__('0-add_integer').add_integer
+add_integer = __import__('../0-add_integer').add_integer
 
 print(add_integer(1, 2))
 print(add_integer(100, -2))
