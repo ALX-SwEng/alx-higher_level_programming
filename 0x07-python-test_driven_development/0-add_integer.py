95da49b80f7d90bb357c941542d95c2da75c7b4b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-21 14:38:19 +0300

    0x07. Python - Test-driven development Task 1 commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """return the sum of two integers 
+
+    if Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer!")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer!")
+    
+    return (int(a) + int(b))
