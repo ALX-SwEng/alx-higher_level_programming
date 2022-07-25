1134122f536432c36e105c5d7b04b24529efeadd
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-25 12:36:26 +0300

    0x08-python-more_classes project tasks commit

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -85,6 +85,6 @@ class Rectangle:
         rect = []
         for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
-            if i != self._height - 1:
+            if i != self.__height - 1:
                 rect.append('\n')
         return ("".join(rect))
