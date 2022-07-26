c2ee2a166f4530cad8f1f8f1ee7e15adf811b750
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-26 18:58:31 +0300

    0x09-python-everything_is_object project tasks commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle"""
+"""Defines a locked class."""
 
 
 class LockedClass:
-    """Represents a rectangle. No body."""
-    lock = threading.Lock()
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
