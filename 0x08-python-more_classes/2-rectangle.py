2658ccb558a6f54cb4fcb450267470b30c29704c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-26 11:54:42 +0300

    0x08-python-more_classes project tasks commit

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,4 +51,4 @@ class Rectangle:
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+        return (2*(self.__width + self.__height))
