60308965cac24866f696110bdc770344bbd7f057
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-26 11:43:49 +0300

    0x08-python-more_classes project tasks commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Defines a class Rectangle."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Represents a rectangle. No body."""
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -17,11 +17,19 @@ class Rectangle:
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Return width of the rectangle """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set width of the rectangle
+
+        Args:
+            value (int): must be a +ve integer.
+        Raises:
+            TypeError exception: If value is not an integer.
+            ValueError exception: if value is less than zero.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -30,11 +38,20 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """Return height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set height of the rectangle 
+
+        Args:
+            value (int): must be a +ve integer
+
+        Raises:
+            TypeError exception: If value is not an integer.
+            ValueError exception: if value is less than zero
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
