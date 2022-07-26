7feaf90dbe9e698d2fa7624191e680c480ec017d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-27 00:36:22 +0300

    0x07. Python - Test-driven development Tasks commit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,16 +16,17 @@ def matrix_divided(matrix, div):
     Returns:
         A new matrix representing the result of the division.
     """
-
     if (not isinstance(matrix, list) or matrix == [] or 
             not all (isinstance(ele, int) or isinstance(ele, float)) 
                 for ele in [num for row in matrix for num in row]):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
     if (not all (len(row) == len[matrix[0]]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
     if ((not isinstance(matrix, int) and not isinstance(matrix, float))):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError ("division by zero")
 
