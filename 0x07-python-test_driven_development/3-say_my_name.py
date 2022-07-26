d58dc9eabb8ceb2af135717b3122671ae80e96d1
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-27 00:54:47 +0300

    0x07. Python - Test-driven development Tasks commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,8 +6,8 @@ def say_my_name(first_name, last_name=""):
     """Print full name as My name is <first name> <last name>.
 
     Args:
-        first_name: a sirng, the first name to print.
-        last_name: a string, the last name to print.
+        first_name (str): the first name to print.
+        last_name (str): the last name to print.
     Raises:
         TypeError: If first name and last name is not string.
     """
