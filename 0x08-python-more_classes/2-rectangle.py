5790cb567b882ab3bd33ae194232d65a2e12be02
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-26 11:53:44 +0300

    0x08-python-more_classes project tasks commit

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,12 +43,12 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    def area(self): 
+    def area(self):
         """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
-    def perimeter(self): 
+    def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
-        return (2 * (self.__width + self.__height))
+        return ((self.__width * 2) + (self.__height * 2))
