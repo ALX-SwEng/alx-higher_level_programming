827aab8c01967970198ab954a41e0e1009008a3d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-27 12:38:41 +0300

    0x07. Python - Test-driven development Tasks commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -16,6 +16,6 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    if first_name:
+    if first_name and last_name:
         space = " "
     print("My name is {}{}{}".format(first_name, space, last_name))
