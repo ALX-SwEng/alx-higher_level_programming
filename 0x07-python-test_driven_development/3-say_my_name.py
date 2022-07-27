ee2774e8ae460920d6de7910bcc53838523c9225
Chere Lemma <chere.lemma@aastu.edu.et>
2022-07-27 12:35:53 +0300

    0x07. Python - Test-driven development Tasks commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -19,5 +19,5 @@ def say_my_name(first_name, last_name=""):
     if first_name:
         space = " "
     if not last_name:
-        space = ""
+        space = " "
     print("My name is {}{}{}".format(first_name, space, last_name))
