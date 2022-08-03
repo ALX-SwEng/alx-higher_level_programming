fd6429ef374c52c1314ac99453cdc3abde4817e8
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-04 02:38:55 +0300

    0x0A. Python - Inheritance project tasks commit

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
 
     def area(self):
         """Not yet implemented."""
-        raise Exception("area() is not implemented yet")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
@@ -19,7 +19,7 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
