b128d10cc704e61cc7ec30f481b178e9e2e5816d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-04 02:54:54 +0300

    0x0A. Python - Inheritance project tasks commit

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
