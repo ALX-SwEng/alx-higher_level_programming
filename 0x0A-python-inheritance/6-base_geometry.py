3a4cabb4bf5eb84dd179cb423d245d8c3fd89135
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-04 02:36:21 +0300

    0x0A. Python - Inheritance project tasks commit

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
 
     def area(self):
         """Not implemented."""
-        raise Exception("area() is not implemented yet")
+        raise Exception("area() is not implemented")
