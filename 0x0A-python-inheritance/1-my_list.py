70f86c01c2adbe162dbe761d163122e111c9367b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-04 01:57:46 +0300

    0x0A. Python - Inheritance project tasks commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,9 @@ contains the MyList class
 
 class MyList(list):
     """a subclass of list"""
-    def __init__(self):
+    def __init__(self, lists=None):
         """initializes the object"""
-        super().__init__()
+        super().__init__(lists)
 
     def print_sorted(self):
         """prints the sorted list"""
