6397e55969a60d977624db8b5b8c3676b7116d86
Chere Lemma <chere.lemma@aastu.edu.et>
2022-08-04 01:35:09 +0300

    0x0A. Python - Inheritance project tasks commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
+"""
+contains the MyList class
+"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
+        """prints the sorted list"""
         print(sorted(self))
