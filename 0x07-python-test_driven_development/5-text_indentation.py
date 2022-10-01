0fb788ac3d31f6ed4f70e1a4ab3aeb7542bf6841
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-02 01:56:42 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,17 +12,9 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    flag = 0
-    for a in text:
-        if flag == 0:
-            if a == ' ':
-                continue
-            else:
-                flag = 1
-        if flag == 1:
-            if a == '?' or a == '.' or a == ':':
-                print(a)
-                print()
-                flag = 0
-            else:
-                print(a, end="")
+    endings = [".", "?", ":"]
+    for i in text:
+        if i in endings:
+            print(f"{i}\n")
+        else:
+            print(i, end="")
