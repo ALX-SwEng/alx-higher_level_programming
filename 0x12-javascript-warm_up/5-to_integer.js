c5503167c8d4dfcedd9af6f288bbcbaac9664b54
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-03 18:02:33 +0300

    0x12-javascript-warm_up Task5 commit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const num = Number(process.argv[2]);
-if (isNaN(num)){
+const num = Number (process.argv[2]);
+if (isNaN(num)) {
   console.log ('Not a number');
-} else{
+} else {
     console.log (`My number: ${num}`);
 }
