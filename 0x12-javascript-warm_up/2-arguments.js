b4c88528cd23458bd87071b32520e3e442a073d4
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-03 16:24:41 +0300

    0x12-javascript-warm_up Task2 commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+
+if (process.argv.length == 2)
+  console.log('No argument');
+else if (process.argv.length == 3) 
+  console.log ('Argument found')
+else
+  console.log ('Arguments found');
