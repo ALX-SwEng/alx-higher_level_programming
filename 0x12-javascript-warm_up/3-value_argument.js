7d88b82fb52d581a1478728d91b3124bf9eaeb70
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-03 17:32:43 +0300

    0x12-javascript-warm_up Task3 commit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-if (typeof process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
