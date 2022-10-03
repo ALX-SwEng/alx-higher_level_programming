5e083845433a604cd8ba42478cea89126508bf83
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-03 16:25:51 +0300

    0x12-javascript-warm_up Task2 commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length == 2)
-  console.log('No argument');
+  console.log ('No argument');
 else if (process.argv.length == 3) 
   console.log ('Argument found')
 else
