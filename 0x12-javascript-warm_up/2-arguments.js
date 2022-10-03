5402de5f594a88823cad1d7e4481869e86f6239e
root <root@DESKTOP-OSDVMDS.localdomain>
2022-10-03 16:37:21 +0300

    0x12-javascript-warm_up Task2 commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv.length === 2) {
-  console.log ('No argument');
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-  
-else if (process.argv.length === 3) {
-  console.log ('Argument found');
-}
-  
-else {
-  console.log ('Arguments found');
-}
-  
