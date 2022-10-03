0ff005a0c8f6badbde1e9e51753ffc032376f235
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-03 19:18:30 +0300

    0x12-javascript-warm_up Task10-13 commit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const num = Number(process.argv[2]);
+console.log(factorial(mum));
