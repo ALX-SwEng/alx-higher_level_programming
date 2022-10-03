aefa9566fbd899792c69ecc19f05fd3e9c3092e2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-03 19:04:20 +0300

    0x12-javascript-warm_up Task6 commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const myvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const str in myvar) {
-  console.log(str);
+for (let i = 0; i < 3; ++i) {
+  console.log(myvar[i]);
 }
