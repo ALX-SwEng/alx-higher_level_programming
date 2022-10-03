85a08ebafd9259380179c1ae94dfa9a187207e62
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-03 18:40:36 +0300

    0x12-javascript-warm_up Task7 commit

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const num = Number(process.argv[2]);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (num > 0) {
+    console.log('C is fun');
+    --num;
+  }
+}
