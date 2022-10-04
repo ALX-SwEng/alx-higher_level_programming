8e71d07f5581d84479d3a3fc928aaa20cda9b997
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-04 23:47:57 +0300

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-module.exports = class Square extends require('./4-rectangle.js') {
+const Rectangle = require('./4-rectangle.js');
+
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
