243f64daba72d25717344ae944045d333a8d20b8
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-04 23:20:17 +0300

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (h > 0 && w > 0){
       [this.height, this.width] = [h, w];
     }
   }
 
-  print (){
-    for (let i = 0; i < this.height; i++){
+  print() {
+    for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate(){
+  rotate() {
     [this.width, this.height] = [this.height, this.width];
   }
 
-  double(){
+  double() {
     this.width *= 2;
     this.height *= 2;
   }
