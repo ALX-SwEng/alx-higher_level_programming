dda50553ff89f57bc3739432ec4228df357a949e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-04 23:23:11 +0300

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (h > 0 && w > 0){
       [this.height, this.width] = [h, w];
     }
   }
 
-  print (){
-    for (let i = 0; i < this.height; i++){
+  print () {
+    for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
