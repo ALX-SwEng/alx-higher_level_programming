2f371d4fcd19db3ae250432585efd540fd06ee0b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-21 04:28:29 +0300

    0x10. Python - Network #0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-#curl -sL -X PUT  -H "Origin: You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
-curl -s -X PUT -H 'Origin: HolbertonSchool' -L --max-redirs -1 -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -sL -X PUT  -H "Origin: You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
