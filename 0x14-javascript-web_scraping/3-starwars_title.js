02e34e954cd14128dbcba0b7f779489962d016a9
cherelemma <73167960+cherelemma@users.noreply.github.com>
2022-12-26 23:25:38 +0300

    JavaScript - Web scraping #Exercise 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
